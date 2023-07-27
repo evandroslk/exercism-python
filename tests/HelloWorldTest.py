613565902a8ca442ade14c4ab44df1272d7497aa
Evandro Lovato Krzyzanovski <evandroslk@gmail.com>
2023-07-27 08:58:23 -0300

    First commit

---
+++ tests/HelloWorldTest.py
@@ -0,0 +1,7 @@
+import unittest
+from exercises.hello_world import hello
+
+
+class HelloWorldTest(unittest.TestCase):
+    def test_say_hi(self):
+        self.assertEqual(hello(), "Hello, World!")
